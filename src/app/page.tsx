3572528a50be56a0586194d5f239ab8ac8383026
Ankitasingh88 <ankita.shibu.singh@gmail.com>
2025-05-27 22:01:36 +0200

    adding button feature/016-redbutton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import RedButton from "@/components/RedButton";
 
 export default function Home() {
   return (
     <>
     <div className={styles.page}>
+       <RedButton />
     </div>
-    <button>click me!!</button>
+     
     </>
   );
 }
---
+++ src/components/RedButton/index.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./redbutton.module.css";
+
+const RedButton = () => {
+  return (
+    <>
+    <button className={styles.button}>click me!!</button>
+    </>
+  );
+}
+export default RedButton
