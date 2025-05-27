e2c2fa78a20f2ac3e7cb7f2540e4e0d892a80824
DenVitaKaninen <tobiaskjernell@gmail.com>
2025-05-27 21:38:50 +0200

    added black bnt component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import BlackBtn from "@/components/BlackBtn";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-       </div>
+      <BlackBtn />
+    </div>
   );
 }
 
